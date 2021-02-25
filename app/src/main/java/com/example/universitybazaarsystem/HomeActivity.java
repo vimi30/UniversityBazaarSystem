de2d749c9d29ec5e05a0712e786021bb77a44d29
Sarah Shaikh <sarahshk37@gmail.com>
2021-02-25 01:27:59 -0600

    Profile Page included

--- app/src/main/java/com/example/universitybazaarsystem/HomeActivity.java
+++ app/src/main/java/com/example/universitybazaarsystem/HomeActivity.java
@@ -2,13 +2,28 @@ package com.example.universitybazaarsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button profileButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        profileButton = (Button) findViewById(R.id.profile);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProfilePage();
+            }
+        });
+    }
+    public void openProfilePage(){
+        Intent intent = new Intent(this, ProfilePage.class);
+        startActivity(intent);
     }
 }
